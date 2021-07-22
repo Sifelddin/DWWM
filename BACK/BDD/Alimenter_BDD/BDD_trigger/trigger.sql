77d94dedb1ac010e9681a669a84c7c48f1298fd2
Bekhouche Ali-Tarek <63245287+Sifelddin@users.noreply.github.com>
2021-07-22 22:13:33 +0200

    'c8'

--- BACK/BDD/Alimenter_BDD/BDD_trigger/trigger.sql
+++ BACK/BDD/Alimenter_BDD/BDD_trigger/trigger.sql
@@ -22,3 +22,18 @@ FROM lignedecommande  WHERE id = id_commande)
 
  CREATE TRIGGER after_products_update after update on products 
  SET qte = qte + 1 from commander_articles 
+
+
+
+ --
+ DELIMITER $$
+ CREATE trigger after_products_update 
+AFTER UPDATE on products for each row 
+begin
+
+IF new.pro_stock < new.pro_stock_alert THEN
+INSERT INTO commander_articles (codArt,qte) 
+VALUES (pro_id, new.pro_stock - new.pro_stock_alert );
+END IF;
+END$$
+DELIMITER ;
